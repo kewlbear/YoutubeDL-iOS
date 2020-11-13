bab40ffe2cc76a620c0775b28043553b3cb46f3f
Changbeom Ahn <kewlbear@gmail.com>
2020-11-13 15:57:36 +0900

    fix package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "YoutubeDL",
+    name: "YoutubeDL-iOS",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
