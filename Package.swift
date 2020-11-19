ae3e326bc55a58d255e9e93e73264f242d10bf9b
Changbeom Ahn <kewlbear@gmail.com>
2020-11-19 11:02:10 +0900

    prepare release

--- Package.swift
+++ Package.swift
@@ -1,30 +1,24 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "YoutubeDL-iOS",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "YoutubeDL",
             targets: ["YoutubeDL"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/kewlbear/FFmpeg-iOS.git", .branch("main")),
-        .package(url: "https://github.com/kewlbear/PythonKit.git", .branch("master")),
-        
+        .package(url: "https://github.com/kewlbear/FFmpeg-iOS.git", from: "0.0.1"),
+                 .package(url: "https://github.com/kewlbear/PythonKit.git", from: "0.0.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YoutubeDL",
             dependencies: ["PythonKit", "FFmpeg-iOS"]),
-//        .testTarget(
-//            name: "YoutubeDLTests",
-//            dependencies: ["YoutubeDL"]),
+        .testTarget(
+            name: "YoutubeDLTests",
+            dependencies: ["YoutubeDL"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# YoutubeDL
+# YoutubeDL-iOS
 
-A description of this package.
+This swift package enables you to use youtube-dl in your iOS apps.
+
+## Warning
+
+This package is NOT AppStore-safe.  Historically AppStore has been removing apps downloading videos from YouTube.  Your app will likely be rejected if you include this package in your app.
+
+## Installation
+
+```
+.package(url: "https://github.com/kewlbear/YoutubeDL-iOS.git", from: "0.0.1")
+```
+
+## Usage
+
+See https://github.com/kewlbear/YoutubeDL.
+
+## Credits
+
+This package uses youtube-dl Python module from https://github.com/ytdl-org/youtube-dl.
+
+## License
+
+MIT
